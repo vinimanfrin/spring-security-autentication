6057ebb5ab5ae36a9feb53414c763301370970a2
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-02 19:47:24 -0300

    criação do projeto

---
+++ src/main/java/com/vinimanfrin/autentication/AutenticationApplication.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.autentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinimanfrin/autentication/AutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.autentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
