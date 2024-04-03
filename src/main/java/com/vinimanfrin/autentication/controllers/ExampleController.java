5f972b5363c82648a5a3cea7ff38205fabd3249a
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-03 17:13:19 -0300

    Implentação dos métodos que simulam requisições com autenticação e autorização do controller de exemplo

--- src/main/java/com/vinimanfrin/autentication/controllers/ExampleController.java
+++ src/main/java/com/vinimanfrin/autentication/controllers/ExampleController.java
@@ -9,10 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/example")
 public class ExampleController {
 
-    @GetMapping("/hello") //End point que não requer nenhuma autenticação
+    @GetMapping("/hello") //simula um end point que não requer nenhuma autenticação
     public ResponseEntity helloWorld(){
         var message = "Hello World";
         return ResponseEntity.ok(message);
     }
 
+    @GetMapping("/private") //simula um end point que requer autenticação
+    public ResponseEntity privateRoute(){
+        var message = "Esse é o método que requer autenticação";
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/admin") //simula um end point que requer autenticação e permissão de administrador
+    public ResponseEntity adminRoute(){
+        var message = "Esse é o método que requer autenticação e permissão de admin";
+        return ResponseEntity.ok(message);
+    }
 }
