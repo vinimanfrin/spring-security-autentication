3d546250ccb81164969b81a35ba2892a2251336d
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-03 17:28:21 -0300

    Criação da entidade User que representa os usuários cadastrados no banco de dados

---
+++ src/main/java/com/vinimanfrin/autentication/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.vinimanfrin.autentication.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String login;
+    private String password;
+
+}
