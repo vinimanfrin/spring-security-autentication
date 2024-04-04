e4819ad1086934335d7e605dff0dceaac95b47f5
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-04 17:20:42 -0300

    Desabilitando a configuração padrão do Spring Security

---
+++ src/main/java/com/vinimanfrin/autentication/infra/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.vinimanfrin.autentication.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
