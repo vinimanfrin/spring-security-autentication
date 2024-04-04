7c51103a532c2c15b16eeb0e01610e1930479999
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-04 17:10:54 -0300

    Criação do serviço AuthorizationService

---
+++ src/main/java/com/vinimanfrin/autentication/infra/security/service/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.vinimanfrin.autentication.infra.security.service;
+
+import com.vinimanfrin.autentication.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    //Esse serviço será chamado automaticamente pelo spring security, por isso necessário implementar a classe UserDetailsService
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
