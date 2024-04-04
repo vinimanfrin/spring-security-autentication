03de071f1637021abfda25a8b428ad24e88b5b0c
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-04-04 17:57:45 -0300

    implementação do securityFilterChain

--- src/main/java/com/vinimanfrin/autentication/infra/security/SecurityConfiguration.java
+++ src/main/java/com/vinimanfrin/autentication/infra/security/SecurityConfiguration.java
@@ -1,9 +1,27 @@
 package com.vinimanfrin.autentication.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/example/hello").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/example/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
